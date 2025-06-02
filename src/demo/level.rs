0d0fc39d79c616f022ca9ca5902afcc015576f56
jbuehler23 <jbuehler23@gmail.com>
2025-06-02 12:08:50 +0100

    feat: add static level bounds for now

--- src/demo/level.rs
+++ src/demo/level.rs
@@ -1,6 +1,7 @@
 //! Spawn the main level.
 
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
+use bevy_inspector_egui::egui::collapsing_header;
 use bevy_rapier2d::prelude::{Collider, RigidBody};
 
 use crate::{asset_tracking::LoadResource, demo::player::player, screens::Screen};
@@ -33,7 +34,13 @@ pub fn spawn_level(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
+    let window = window_query.single().expect("PrimaryWindow not found");
+
+    // Spawn screen bounds first
+    spawn_screen_bounds(&mut commands, window);
+
     let atom_radius = 50.0;
     // let num_parts = 6;
     // let part_radius = 10.0;
@@ -54,7 +61,12 @@ pub fn spawn_level(
                 &mut materials
             ),
             obstacle(Vec2 { x: 100.0, y: 0.0 }, 50.0, &mut meshes, &mut materials),
-            atom_seed(Vec2 { x: -100.0, y: 0.0 }, atom_radius, &mut meshes, &mut materials),
+            atom_seed(
+                Vec2 { x: -100.0, y: 0.0 },
+                atom_radius,
+                &mut meshes,
+                &mut materials
+            ),
         ],
     ));
 }
@@ -75,4 +87,33 @@ fn obstacle(
         RigidBody::Fixed,
         Collider::cuboid(size / 2.0, size / 2.0),
     )
+}
+
+fn spawn_screen_bounds(commands: &mut Commands, window: &Window) {
+    let width = window.width();
+    let height = window.height();
+    commands.spawn((
+        Name::new("Left Wall"),
+        Transform::from_xyz(-width / 2.0, 0.0, 0.0),
+        RigidBody::Fixed,
+        Collider::cuboid(1.0, height / 2.0),
+    ));
+    commands.spawn((
+        Name::new("Right Wall"),
+        Transform::from_xyz(width / 2.0, 0.0, 0.0),
+        RigidBody::Fixed,
+        Collider::cuboid(1.0, height / 2.0),
+    ));
+    commands.spawn((
+        Name::new("Top Wall"),
+        Transform::from_xyz(0.0, height / 2.0, 0.0),
+        RigidBody::Fixed,
+        Collider::cuboid(width / 2.0, 1.0),
+    ));
+    commands.spawn((
+        Name::new("Bottom Wall"),
+        Transform::from_xyz(0.0, -height / 2.0, 0.0),
+        RigidBody::Fixed,
+        Collider::cuboid(width / 2.0, 1.0),
+    ));
 }
